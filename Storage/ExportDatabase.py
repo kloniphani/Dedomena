92325f171eb8bd616f7fd4acf1a2318b68546bbe
Kloniphani Maluleke <kloniphani@gmail.com>
2018-12-06 13:53:30 +0100

    Created a database extractor

---
+++ Storage/ExportDatabase.py
@@ -0,0 +1,13 @@
+import pyrebase
+
+Config = {
+    "apiKey": "AIzaSyCYu7gE_4HGDIy7pOOiw0AY-rrUmoE7eXQ",
+    "authDomain": "sensehat-51bd7.firebaseapp.com",
+    "databaseURL": "https://sensehat-51bd7.firebaseio.com",
+    "storageBucket": "sensehat-51bd7.appspot.com"
+}
+
+Firebase = pyrebase.initialize_app(Config)
+db = Firebase.database()
+
+print(db)
