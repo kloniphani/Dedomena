874379c7fa63979c58799c4b8d7aa7238861cd46
tawandamatsika39 <44851355+tawandamatsika39@users.noreply.github.com>
2018-12-06 13:05:23 +0200

    Update SensorPubNub.py

--- Sensor/SensorPubNub.py
+++ Sensor/SensorPubNub.py
@@ -1,6 +1,3 @@
-from Phidgets.PhidgetException import *
-from Phidgets.Events.Events import *
-from Phidgets.Devices.InterfaceKit import *
 from pubnub.pubnub import PubNub, SubscribeListener, SubscribeCallback, PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.exceptions import PubNubException
@@ -15,7 +12,6 @@ from sense_hat import SenseHat
 sense = SenseHat()
 pnconf = PNConfiguration()  
 sleep(2)
-
 
 pnconf.publish_key = 'pub-c-23235607-897e-4e8c-8b96-409d2a0ce710'       # set pubnub publish_key
 pnconf.subscribe_key = 'sub-c-7b8840c4-f940-11e8-ba8a-aef4d14eb57e'     # set pubnub subscibe_key
@@ -44,7 +40,6 @@ while(1):
 	humidity = round((sense.get_humidity()*64)/100,1)
 	temperature = round(sense.get_temperature(),1)
 	pressure = round(sense.get_pressure(),1)
-
 	
 	pubnub.publish().channel(channel).message(
         	{"eon":{
@@ -54,16 +49,12 @@ while(1):
 		}}
     	).async(my_publish_callback)
 	
-	
-
 	print
 	print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 	print("Ambient Temperature: " + str(temperature) + " degrees Celcius")
 	print("Relative Humidity: " + str(humidity) + "%")
 	print("Pressure: " + str(pressure) + " bars")
-
 	sleep(5)
-
 
 while True:                                                 # Infinite loop
 	result = my_listener.wait_for_message_on(channel)       # Read the new msg on the channel
