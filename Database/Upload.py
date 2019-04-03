042f4736eb0c3c3c3fd2baf36ff2c4c6d3575c44
kloniphani <kloniphani@gmail.com>
2019-04-03 16:22:00 +0200

    Testing pushingEnvironmental data v1.0.4

--- Database/Upload.py
+++ Database/Upload.py
@@ -59,6 +59,7 @@ class Upload(object):
 							"model STRING, " \
 							"ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE LOCATION '/test-warehouse/data/sensor';"
 					self.IMPALA_CONNECTION.Execute(Query)
+					print("---------------------------------------------")
 
 					Query = "INSERT INTO dedomena.device (macAddress, manufacturer, model) VALUES({0}, {1}, {2});".format(self.MacAddress, 'Raspberry Pi', 'Model B+');
 					#self.IMPALA_CONNECTION.Execute(Query)
