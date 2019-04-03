1f5b7295985985b24381513124e7f8b260973b68
kloniphani <kloniphani@gmail.com>
2019-04-03 17:34:40 +0200

    Testing pushingEnvironmental data v1.0.19

--- Database/Connection.py
+++ Database/Connection.py
@@ -1,48 +1,48 @@
 class Connection(object):
-	"""description of class"""
-	
-	CONNECTION =  None; 
+    """description of class"""
 
-	def Impala(self, Daemon, Port = 21050):
-		from impala import dbapi;
-		from sys import exc_info; 
+    CONNECTION =  None;
 
-		try:
-			self.CONNECTION = dbapi.connect(Daemon, Port)
-			print("+Successfully connected to: {0} with port: {1}\n\n".format(Daemon, Port))
-		except Exception as e:
-			print("Unexpected error on function: {0}\nDetails:\t{1}".format("Impala", e)
+    def Impala(self, Daemon, Port = 21050):
+        from impala import dbapi;
+        from sys import exc_info;
 
-		return self.CONNECTION;
+        try:
+            self.CONNECTION = dbapi.connect(Daemon, Port)
+            print("+Successfully connected to: {0} with port: {1}\n\n".format(Daemon, Port))
+        except Exception as e:
+            print("Unexpected error on function: {0}\nDetails:\t{1}".format("Impala", e))
 
-	def Hive(self, Server, Port = 10000):
-		from pyhive import hive;
-		from sys import exc_info;
+        return self.CONNECTION;
 
-		try:
-			self.CONNECTION = hive.connect(Server, Port)
-			print("+Successfully connected to: {0} with port: {1}\n\n".format(Server, Port))
-		except Exception as e:
-			print("Unexpected error on function: {0}\nDetails:\t{1}".format("Hive", e)
-		
-		return self.CONNECTION;
+    def Hive(self, Server, Port = 10000):
+        from pyhive import hive;
+        from sys import exc_info;
 
-	def Execute(self, Query, Fetch = 'None'):
-		from sys import exc_info;
+        try:
+            self.CONNECTION = hive.connect(Server, Port)
+            print("+Successfully connected to: {0} with port: {1}\n\n".format(Server, Port))
+        except Exception as e:
+            print("Unexpected error on function: {0}\nDetails:\t{1}".format("Hive", e))
 
-		if self.CONNECTION is not None:
-			try:
-				cursor = self.CONNECTION.cursor()
-				cursor.execute(Query)
+        return self.CONNECTION;
 
-				if(Fetch.lower() == 'none'):
-					return None
-				elif(Fetch.lower() == 'all' and cursor != None):
-					return cursor.fetchall()
-				else:
-					return cursor.fetchone()
-			except Exception as e:
-				print("Unexpected error on function: {0}\Details:\t{1}".format("Excute", e)
-		else:
-			print("!Not connected to any database")
-		return None
+    def Execute(self, Query, Fetch = 'none'):
+        from sys import exc_info;
+
+        if self.CONNECTION is not None:
+            try:
+                cursor = self.CONNECTION.cursor()
+                cursor.execute(Query)
+
+                if(Fetch.lower() == 'none'):
+                    return None
+                elif(Fetch.lower() == 'all' and cursor != None):
+                    return cursor.fetchall()
+                else:
+                    return cursor.fetchone()
+            except Exception as e:
+                print("Unexpected error on function: {0}\nDetails:\t{1}".format("Excute", e))
+        else:
+            print("!Not connected to any database")
+        return None
